2c0bef42216839fe0c06762d056c238f80ff8d4b
Grognor <Rugall1998@gmail.com>
2022-03-10 01:04:33 +0100

    Inventory Logic

--- Tlaloc-Manuscript-Demo/Assets/Inventory/Scripts/Inventory.cs
+++ Tlaloc-Manuscript-Demo/Assets/Inventory/Scripts/Inventory.cs
@@ -2,33 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory")]
+[CreateAssetMenu(fileName = "New Inventory", menuName = "InventorySysytem/Inventory")]
 public class Inventory : ScriptableObject
 {
-    [SerializeField]
-    private ItemIntDictionary container = new ItemIntDictionary();
+    public List<InventorySlot> container = new List<InventorySlot>();
 
-    private int basicSlots = 20;
-
-    public void AddItem(Item itemToAdd,int amount) {
-        foreach (var item in container) {
-            if (item.Key.iD == itemToAdd.iD) {
-                container[itemToAdd]++;
+    public void AddItem(Item _item,int _amount) {
+        bool hasItem = false;
+        for (int i = 0; i < container.Count; i++) {
+            if (container[i].item.iD == _item.iD) {
+                container[i].AddAmount(_amount);
+                hasItem = true;
                 break;
             }
-            else {
-                container.Add(itemToAdd,amount);
-            }
         }
-    }
-
-    public void AddItem(Item itemToAdd) { 
-    
+        if (!hasItem) {
+            container.Add(new InventorySlot(_item, _amount));
+        }
     }
 }
 
 
 
-
 [System.Serializable]
-public class ItemIntDictionary : SerializableDictionary<Item, int> { }
+public class InventorySlot {
+    public Item item;
+    public int amount;
+    public InventorySlot(Item _item,int _amount) {
+        item = _item;
+        amount = _amount;
+    }
+
+    public void AddAmount(int value) {
+        amount += amount;
+    }
+}
